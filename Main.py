3b4bf836a5f3b90cd14242b285fbe13e26d42683
jcp99gamers <jcp99gamer@gmail.com>
2023-02-28 01:52:43 +0530

    Created a Code to Download the Latest Version of the Requirments for the Project In the Case that the User Doesnt Have Any.

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
+import subprocess
+import pkg_resources
+
+# Read the requirements.txt file and extract the library names and versions
+with open('requirements.txt') as f:
+    libs = [line.strip() for line in f]
+
+# Check if each library exists and is up to date, and install/update it if necessary
+for i, lib in enumerate(libs):
+    try:
+        pkg_resources.require(lib)
+    except (ImportError, pkg_resources.VersionConflict):
+        subprocess.check_call(['pip', 'install', '-U', lib])
+        # Update the requirements.txt file with the latest version of the installed library
+        dist = pkg_resources.get_distribution(lib)
+        libs[i] = '{}=={}'.format(dist.project_name, dist.version)
+
+# Write the updated requirements.txt file
+with open('requirements.txt', 'w') as f:
+    f.write('\n'.join(libs))
+
+
 import UseTable
